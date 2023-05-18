786fe2cc4c3ca189e09e36c28d75dcdb49c2aa64
AbdlrhmanSaid <bodesaid3@gmail.com>
2023-05-18 14:03:01 +0300

    added edit in js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+let login = document.querySelector("nav .log");
+
+login.addEventListener("click", function () {
+  document.querySelector(".log-in").style.transform = "translateY(0%)";
+  let btn = document.querySelector("form button");
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let inputs = document.querySelectorAll("form input");
+    Array.from(inputs).forEach((e) => {
+      if (e.value === "") {
+        e.style.borderBottom = "1px solid red";
+        document.querySelector(".log-in").style.transform = "translateY(0%)";
+      } else {
+        e.style.borderBottom = "1px solid white";
+        document.querySelector(".log-in").style.transform = "translateY(-100%)";
+      }
+    });
+  });
+  document
+    .querySelector(".log-in .close")
+    .addEventListener("click", function () {
+      document.querySelector(".log-in").style.transform = "translateY(-100%)";
+    });
+});
+
 let tabs = document.querySelectorAll(".tabs li");
 let tabsArray = Array.from(tabs);
 let divs = document.querySelectorAll(".content div");
@@ -5,22 +30,30 @@ let divsArray = Array.from(divs);
 
 tabsArray.forEach((el) => {
   el.addEventListener("click", function (e) {
+    // remove active class
     tabsArray.forEach((ele) => {
-      // remove active class
       ele.classList.remove("active");
     });
     // add active class
     e.currentTarget.classList.add("active");
-    // divsArray.forEach((div) => {
-    //   div.style.display = "none";
+
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+
+    let select = document.querySelectorAll(e.currentTarget.dataset.cont);
+
+    let selectArry = Array.from(select);
+
+    selectArry.forEach((e) => {
+      e.style.display = "block";
+    });
+
+    let All = document.querySelector(".all");
+    All.addEventListener("click", function () {
+      divsArray.forEach((div) => {
+        div.style.display = "block";
+      });
+    });
   });
-  // document.querySelector(e.currentTarget.dataset.cont).style.display =
-  //   "block";
-  // let All = document.querySelector(".all");
-  // All.addEventListener("click", function () {
-  //   divsArray.forEach((div) => {
-  //     div.style.display = "block";
-  //   });
-  // });
 });
-// });
